6af1187282c503889a906eb58ab969bf75a2dd4f
carlosdiaz3274 <carlosdiaz.app@gmail.com>
2021-07-29 14:23:27 -0600

    comando insert_one

--- connection1.py
+++ connection1.py
@@ -5,15 +5,19 @@ ca = certifi.where()
 
 client = pymongo.MongoClient("mongodb+srv://jcdiaz:cd119105@cluster0.6ue2j.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", tlsCAFile=ca)
 
-db = client.test
+#db = client.test
 
-print(client.list_database_names()) #show databases en SQL
+#print(client.list_database_names()) #show databases en SQL
 
-mydb = client["sample_restaurants"] #se accede a la bd
+mydb = client["mydatabase"] #se crea la base de datos mydatabase
+mycol = mydb["equipos"] #se crea una coleccion
+mydic = { "_id": 3,  "name": "Aguila", "mascota": "Aguila", "departamento":"San Miguel", "integrantes": 22 } #preparamos los datos a ingrear
+#print(mydb.list_collection_names())
+x = mycol.insert_one(mydic) #ingresamos los datos
 
-print(mydb.list_collection_names())
+print(x)
 
-mycol = mydb["customers"]
+
 
 
 
