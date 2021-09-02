eb2e66134795f5aa5fc57e77432e4a83f63c9098
carlosdiaz3274 <carlosdiaz.app@gmail.com>
2021-09-02 15:49:30 -0600

    prueba de subir datos desde cvs a atlas

--- desert3.py
+++ desert3.py
@@ -2,3 +2,13 @@ import conexion as net
 import pandas as pd
 
 
+mydb = net.client["mydatabase"]
+mycol = mydb["desert"]
+df = pd.read_csv("pythonymongodb-1\data\surveys.csv", delimiter=',')
+df10 = df.head(1000)
+for i in df10.index:
+    print("Total income in ", df["sex"][i])
+
+df10.reset_index(inplace=True)
+data_dict = df10.to_dict("records")
+mycol.insert_many(data_dict)
